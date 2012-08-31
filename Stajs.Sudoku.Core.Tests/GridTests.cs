ae6567421b4410206ab4ad1860e1bbdb9c06677c
Shannon Skinner <shannonsnz@gmail.com>
2012-08-31 21:18:01 +1200

    Added Grid.
    Let's try some TDD.

---
+++ Stajs.Sudoku.Core.Tests/GridTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stajs.Sudoku.Core.Tests
+{
+	[TestClass]
+	public class GridTests
+	{
+
+		#region Constructor
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Ctor_Null_ThrowsArgumentNullException()
+		{
+			new Grid(null);
+		}
+
+		#endregion
+	}
+}
---
+++ Stajs.Sudoku.Core/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stajs.Sudoku.Core
+{
+	public class Grid
+	{
+		internal int[,] Values;
+
+		public Grid(int[,] values)
+		{
+			if (values == null) throw new ArgumentNullException();
+		}
+	}
+}
