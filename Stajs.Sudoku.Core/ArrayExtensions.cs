f241b6d43a116667574ef1cbfb9f1cb4e4149a13
Shannon Skinner <shannonsnz@gmail.com>
2012-09-01 13:13:19 +1200

    Slicing

---
+++ Stajs.Sudoku.Core/ArrayExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Stajs.Sudoku.Core
+{
+	internal static class ArrayExtensions
+	{
+		private enum Dimension
+		{
+			Row,
+			Column
+		}
+
+		internal static int[] GetRow(this int[,] source, int index)
+		{
+			return Slice(Dimension.Row, source, index);
+		}
+
+		internal static int[] GetColumn(this int[,] source, int index)
+		{
+			return Slice(Dimension.Column, source, index);
+		}
+
+		private static int[] Slice(Dimension dimension, int[,] source, int index)
+		{
+			var length = source.GetLength(dimension == Dimension.Row ? 0 : 1);
+			var ret = new int[length];
+
+			for (int i = 0; i < length; i++)
+				ret[i] = dimension == Dimension.Row
+					? source[index, i]
+					: source[i, index];
+
+			return ret;
+		}
+	}
+}
